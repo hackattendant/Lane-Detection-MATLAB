a24ec73e1f5cbec34522b956be9ff38f903c560a
Ethan Peacock <peacockethan@gmail.com>
2019-11-25 18:04:42 -0500

    Added to playground

--- playground.m
+++ playground.m
@@ -1,4 +1,22 @@
 % read in image
 img = imread('test_images/test1.jpg');
 % display
+figure();
 imshow(img);
+title('Original Image');
+hold off;
+
+% threshold image
+threshed = threshold(img);
+figure();
+imshow(threshed);
+title('Thresholded Image');
+hold off;
+
+figure();
+imshow(img); hold on;
+l = plot(740, 444, 'ro'); hold on; l.MarkerFaceColor = l.Color;
+l = plot(600, 444, 'ro'); hold on; l.MarkerFaceColor = l.Color;
+l = plot(1200, 720, 'bo'); hold on; l.MarkerFaceColor = l.Color;
+l = plot(180, 720, 'bo'); hold on; l.MarkerFaceColor = l.Color;
+title('points for perspective transform and masking');
