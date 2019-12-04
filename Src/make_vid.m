6ed326ca5f20b4e01145dc0ea3f09cfa616fe462
Ethan Peacock <peacockethan@gmail.com>
2019-12-04 01:12:52 -0500

    removed first_pass flag from make_vid since it was removed from pipes()

--- Src/make_vid.m
+++ Src/make_vid.m
@@ -1,19 +1,16 @@
 % Generates marked up output video from driving input video.
 % read in video
-v = VideoReader('../Video/short_test.mp4');
+v = VideoReader('../Videos/driving_vid.mp4');
 % create output video and open it
-out = VideoWriter('../Videos/short_out_vid.mp4', 'MPEG-4');
+out = VideoWriter('../Videos/status_driving_vid', 'MPEG-4');
 open(out);
-% set pipes first pass flag to true for first call
-first_pass = true;
+
 % initialize empty left and right lanes to be used on first pass
 left = []; right = [];
 while hasFrame(v)
     frame = readFrame(v);
-    [X, left, right] = pipes(frame, left, right, first_pass);
+    [X, left, right] = pipes(frame, left, right);
     writeVideo(out, X);  % write output frame
-    % change the pipes first_pass flag to false for all other calls
-    first_pass = false; 
 end
 % close the output video
 close(out);
