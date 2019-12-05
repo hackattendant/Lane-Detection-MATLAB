c72e7734ed32e5f5d48eeb462082369bd7ca55c4
Ethan Peacock <peacockethan@gmail.com>
2019-12-05 14:20:34 -0500

    Changed make_vid to run code on short test video

--- Src/make_vid.m
+++ Src/make_vid.m
@@ -4,10 +4,10 @@
 t = cputime;
 
 % read in video
-v = VideoReader('../Videos/driving_vid.mp4');
+v = VideoReader('../Videos/short.mp4');
 
 % create output video and open it
-out = VideoWriter('../Videos/out_driving_vid', 'MPEG-4');
+out = VideoWriter('../Videos/run_test', 'MPEG-4');
 open(out);
 
 % initialize empty left and right lanes to be used on first pass
@@ -17,7 +17,7 @@ while hasFrame(v)
     % read frame
     frame = readFrame(v);
     % get lines and draw
-    [X, left, right] = pipes(frame, left, right);
+    [X, left, right] = pipeline(frame, left, right);
     % write output frame
     writeVideo(out, X);  
 end
