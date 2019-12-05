85473e6d5cf89d7817f39e0c9ada6b0b9e3f4360
Ethan Peacock <peacockethan@gmail.com>
2019-12-05 14:12:24 -0500

    Cleaned up comments

--- Src/make_vid.m
+++ Src/make_vid.m
@@ -1,25 +1,29 @@
-% Generates marked up output video from driving input video.
-% read in video
+%% Generates marked up output video from driving input video.
 
-
+% for timing
 t = cputime;
 
-
+% read in video
 v = VideoReader('../Videos/driving_vid.mp4');
+
 % create output video and open it
 out = VideoWriter('../Videos/out_driving_vid', 'MPEG-4');
 open(out);
 
 % initialize empty left and right lanes to be used on first pass
 left = []; right = [];
+
 while hasFrame(v)
+    % read frame
     frame = readFrame(v);
+    % get lines and draw
     [X, left, right] = pipes(frame, left, right);
-    writeVideo(out, X);  % write output frame
+    % write output frame
+    writeVideo(out, X);  
 end
+
 % close the output video
 close(out);
-
-
+% calculate and display time
 e = cputime - t;
 disp(e);
