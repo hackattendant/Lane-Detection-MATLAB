7b5a8bd7bf0918e582916b112e1119b4dba0c681
Ethan Peacock <peacockethan@gmail.com>
2019-12-06 12:29:18 -0500

    Commented out video demonstration code and running image demonstration code

--- Src/Demonstrate.m
+++ Src/Demonstrate.m
@@ -1,10 +1,10 @@
 %% Demonstrate on Video
-make_vid('../Videos/short.mp4', '../Videos/run_test', 'MPEG-4')
+% make_vid('../Videos/short.mp4', '../Videos/run_test', 'MPEG-4')
 
 %% Demonstrate on Image
-% v = VideoReader('../Videos/short.mp4');
-% frame = readFrame(v);
+v = VideoReader('../Videos/short.mp4');
+frame = readFrame(v);
 
-% left = []; right = [];
-% [X, left, right] = pipeline(frame, left, right);
-% close all; imshow(X);
+left = []; right = [];
+[X, left, right] = pipeline(frame, left, right);
+close all; imshow(X);
